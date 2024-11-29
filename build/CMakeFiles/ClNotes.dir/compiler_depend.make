ac53c32dfa2f253665c63c9f02b2200bb0ab840f
dmihovch <dmihovch@gmail.com>
2024-11-29 16:27:58 -0500

    first commit

---
+++ build/CMakeFiles/ClNotes.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ClNotes.
+# This may be replaced when dependencies are built.
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "../include/cli.hpp"
+using namespace std;
+int main(int argc, char **argv)
+{
+    cout << "Hello" << endl;
+    return 0;
+}
